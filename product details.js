1c26e0cc609da17ea5ea6b91565d95922257807e
nk960290-ui <nk960290@gmail.com>
2026-06-30 18:26:35 +0530

    Add files via upload

---
+++ product details.js
@@ -0,0 +1,11 @@
+const product = {
+    name: "personal computer",
+    price: 75000,
+    brand:"acer",
+    stock: 20,
+};
+
+const {name, price} = product;
+
+console.log("name :",name);
+console.log("price:",price);
